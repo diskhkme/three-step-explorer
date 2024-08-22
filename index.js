5f38070bc63d2f437350f5139a0132a80fb05d59
kimhxsong <kimhxsong@gmail.com>
2024-08-22 12:21:56 +0900

    feat: add camera, renderer, controls, and helpers to the scene

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import GUI from "lil-gui";
+
+/**
+ * Debug
+ */
+const gui = new GUI();
+
 // local storage
 // TODO: 중복된 이름이 파일 목록에 이미 존재하면 (1), (2), ... 과 같이 파일 이름에 번호를 추가하여 목록에 추가
 document
@@ -45,6 +54,37 @@ window.addEventListener("load", function () {
 });
 
 // Canvas
+const canvas = document.getElementById("canvas");
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  canvas.clientWidth / canvas.clientHeight,
+  0.1,
+  10000
+);
+camera.position.set(500, 1500, 500);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.setSize(canvas.clientWidth, canvas.clientHeight);
 
-// const canvas = document.getElementById("myCanvas");
-// const context = canvas.getContext("2d");
+// Add OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+controls.dampingFactor = 0.25;
+controls.screenSpacePanning = false;
+controls.maxPolarAngle = Math.PI / 2;
+
+// Add a grid plane
+const gridHelper = new THREE.GridHelper(2000, 20);
+scene.add(gridHelper);
+
+// Add a axis helper
+const axesHelper = new THREE.AxesHelper(1000);
+scene.add(axesHelper);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
