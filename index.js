6408b5ebbad18601f89ee60196fa009e4a07dbef
kimhxsong <kimhxsong@gmail.com>
2024-08-24 12:37:28 +0900

    chore: import occt-import-js

--- index.js
+++ index.js
@@ -1,63 +1,12 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { ViewHelper } from "three/examples/jsm/helpers/ViewHelper.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import GUI from "lil-gui";
 
-// local storage
-// TODO: 중복된 이름이 파일 목록에 이미 존재하면 (1), (2), ... 과 같이 파일 이름에 번호를 추가하여 목록에 추가
-document
-  .getElementById("fileInput")
-  .addEventListener("change", function (event) {
-    const fileList = event.target.files;
-    const objectList = document.getElementById("objectList");
-    const existingNames = JSON.parse(localStorage.getItem("fileNames")) || {};
-
-    for (let i = 0; i < fileList.length; i++) {
-      let fileName = fileList[i].name;
-      if (existingNames[fileName]) {
-        let count = existingNames[fileName];
-        fileName = `${fileName} (${count})`;
-        existingNames[fileList[i].name] = count + 1;
-      } else {
-        existingNames[fileList[i].name] = 1;
-      }
-
-      const listItem = document.createElement("li");
-      listItem.textContent = fileName;
-
-      // Create add and remove buttons
-      const addButton = document.createElement("button");
-      addButton.textContent = "Add";
-      addButton.addEventListener("click", function () {
-        // Event to add model to the scene
-        console.log(`Add model: ${fileName}`);
-        // Add your model loading logic here
-      });
-
-      const removeButton = document.createElement("button");
-      removeButton.textContent = "Remove";
-      removeButton.addEventListener("click", function () {
-        // Remove item from list and local storage
-        objectList.removeChild(listItem);
-        localStorage.removeItem(fileName);
-        delete existingNames[fileName];
-        localStorage.setItem("fileNames", JSON.stringify(existingNames));
-      });
-
-      listItem.appendChild(addButton);
-      listItem.appendChild(removeButton);
-      objectList.appendChild(listItem);
-
-      // Save file to local storage
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        localStorage.setItem(fileName, e.target.result);
-      };
-      reader.readAsDataURL(fileList[i]);
-    }
-
-    localStorage.setItem("fileNames", JSON.stringify(existingNames));
-  });
+let camera, scene, renderer;
+let test_model;
 
 const clearButton = document.getElementById("clear");
 clearButton.textContent = "Clear";
@@ -81,8 +30,8 @@ window.addEventListener("load", function () {
 
 // Canvas
 const canvas = document.getElementById("canvas");
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(
   75,
   canvas.clientWidth / canvas.clientHeight,
   0.1,
@@ -90,7 +39,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.set(500, 1500, 500);
 camera.lookAt(0, 0, 0);
-const renderer = new THREE.WebGLRenderer({
+renderer = new THREE.WebGLRenderer({
   canvas,
   antialias: true,
   alpha: true,
@@ -140,8 +89,8 @@ gizmo.style.top = `${clientRect.bottom - helperSize}px`;
 gizmo.style.left = `${clientRect.right - helperSize}px`;
 gizmo.style.right = `${clientRect.right}px`;
 gizmo.style.bottom = `${clientRect.bottom}px`;
-gizmo.style.backgroundColor = "gray";
-gizmo.style.opacity = "0.2";
+gizmo.style.backgroundColor = "0x333333";
+gizmo.style.opacity = "0.4";
 gizmo.style.borderRadius = "50%";
 
 helper.setLabels("X", "Y", "Z");
@@ -153,6 +102,7 @@ gizmo.addEventListener("pointerup", (event) => {
 
 function animate() {
   requestAnimationFrame(animate);
+
   const delta = clock.getDelta();
   if (helper.animating) helper.update(delta);
   renderer.clear();
@@ -161,3 +111,64 @@ function animate() {
 }
 
 animate();
+
+// local storage
+// TODO: 중복된 이름이 파일 목록에 이미 존재하면 (1), (2), ... 과 같이 파일 이름에 번호를 추가하여 목록에 추가
+document
+  .getElementById("fileInput")
+  .addEventListener("change", function (event) {
+    const fileList = event.target.files;
+    const objectList = document.getElementById("objectList");
+    const existingNames = JSON.parse(localStorage.getItem("fileNames")) || {};
+
+    for (let i = 0; i < fileList.length; i++) {
+      let fileName = fileList[i].name;
+      if (existingNames[fileName]) {
+        let count = existingNames[fileName];
+        fileName = `${fileName} (${count})`;
+        existingNames[fileList[i].name] = count + 1;
+      } else {
+        existingNames[fileList[i].name] = 1;
+      }
+
+      const listItem = document.createElement("li");
+      listItem.textContent = fileName;
+
+      // Create add and remove buttons
+      const addButton = document.createElement("button");
+      addButton.textContent = "Add";
+      addButton.addEventListener("click", function () {
+        occtimportjs().then(async function (occt) {
+          console.log(occt);
+        });
+      });
+
+      const removeButton = document.createElement("button");
+      removeButton.textContent = "Remove";
+      removeButton.addEventListener("click", function () {
+        // Remove item from list and local storage
+        objectList.removeChild(listItem);
+        localStorage.removeItem(fileName);
+        delete existingNames[fileName];
+        localStorage.setItem("fileNames", JSON.stringify(existingNames));
+        console.log(scene);
+      });
+
+      listItem.appendChild(addButton);
+      listItem.appendChild(removeButton);
+      objectList.appendChild(listItem);
+
+      // Save file to local storage
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const base64String = btoa(
+          String.fromCharCode(...new Uint8Array(e.target.result))
+        );
+        localStorage.setItem(fileName, base64String);
+        // localStorage.setItem(fileName, e.target.result);
+      };
+      reader.readAsArrayBuffer(fileList[i]);
+    }
+
+    localStorage.setItem("fileNames", JSON.stringify(existingNames));
+  });
